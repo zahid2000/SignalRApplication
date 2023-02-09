080ffacbbc5fa2e7ed4f33aebd69a20025f58685
zahid2000 <zahid.mamedov07@gmail.com>
2023-02-09 23:36:03 +0400

    3 cu ders tamamlandi

--- WebSocket/WebSocket/Hubs/MyHub.cs
+++ WebSocket/WebSocket/Hubs/MyHub.cs
@@ -2,8 +2,25 @@
 
 public class MyHub : Hub
 {
+    static List<string> ClientList=new List<string>();
     public async Task SenMessageAsync(string message)
     {
      await  Clients.All.SendAsync("receiveMessage",message);
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        ClientList.Add(Context.ConnectionId);
+        await Clients.All.SendAsync("clients", ClientList);
+        await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+
+        ClientList.Remove(Context.ConnectionId);
+        await Clients.All.SendAsync("clients", ClientList);
+        await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
+
     }
 }
